c2fc08c01d465a228a3eb0649e7327e993e7907b
F4KHR111 <fakhriwirdiyan05@gmail.com>
2024-05-13 15:30:56 +0700

    Membuat akses modifier bersifat public

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -14,7 +14,7 @@ public:
     void setID();
     void printAll();
 
-    static void setNim(int pNim) { nim+ pNim; }
+    static void setNim(int pNim) { nim = pNim; }
     static int getNim() { return nim; }
 
     mahasiswa(string pnama):nama(pnama)
