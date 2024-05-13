8bf4b0818aee8d1f79256ddcb538849fac077f77
F4KHR111 <fakhriwirdiyan05@gmail.com>
2024-05-13 15:58:29 +0700

    Membuat prosedur utama

--- construktor.cpp
+++ construktor.cpp
@@ -20,4 +20,34 @@ public:
     Mahasiswa(string);
     Mahasiswa(int iNim, string iNama);
     void cetak();
+
+    Mahasiswa::Mahasiswa(string iNama){
+        nama = iNama;
+    }
+
+    Mahasiswa::Mahasiswa(int iNim,string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    };
+
+    void Mahasiswa::cetak(){
+        cout << "NIM = " << nim << endl;
+        cout << "NAMA = " << nama << endl;
+    }
 };
+
+int main()
+{
+    Mahasiswa mhs1();
+    Mahasiswa mhs2(20);
+    Mahasiswa mhs3("Rohman");
+    Mahasiswa mhs4(31, "Arul");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
